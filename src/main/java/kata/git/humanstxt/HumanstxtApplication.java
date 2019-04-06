de05016f0bf1b2a7eec2a0fa29323002b1ab8046
Ben WU <wubinben@icloud.com>
2019-04-06 16:06:48 +0800

    add a spring maven blank project

---
+++ src/main/java/kata/git/humanstxt/HumanstxtApplication.java
@@ -0,0 +1,13 @@
+package kata.git.humanstxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanstxtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanstxtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kata/git/humanstxt/HumanstxtApplicationTests.java
@@ -0,0 +1,16 @@
+package kata.git.humanstxt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HumanstxtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
